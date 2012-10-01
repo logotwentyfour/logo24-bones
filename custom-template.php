51329e7f5fbc025a2a86b7955a14bd05ad6a0ac2
Patrick O'Hanlon <paddy@paddyohanlon.com>
2012-10-01 13:38:53 +0200

    Update default page.php and add blank custom template.

--- custom-template.php
+++ custom-template.php
@@ -1,7 +1,11 @@
-<?php get_header(); ?>
-  
+<?php
+/*
+Template Name: Custom Template
+*/
+get_header(); ?>
+
   <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-    
+
     <section role="main">
       <header>
         <h1><?php the_title(); ?></h1>
