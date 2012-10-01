b1c34a4247352475d490ed2b1d45f772739933f7
Patrick O'Hanlon <paddy@paddyohanlon.com>
2012-10-01 16:36:23 +0200

    Add role to sidebar and update div to section

--- sidebar.php
+++ sidebar.php
@@ -1,4 +1,4 @@
-				<div id="sidebar1">
+				<section id="sidebar1" role="complementary">
 
 					<?php if ( is_active_sidebar( 'sidebar1' ) ) : ?>
 
@@ -14,4 +14,4 @@
 
 					<?php endif; ?>
 
-				</div>
+				</section>
