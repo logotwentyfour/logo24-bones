bd4d22df3bada1c4fb6924dbcdc51bba3893363e
Patrick O'Hanlon <paddy@paddyohanlon.com>
2012-12-26 09:21:47 +0100

    Word on the comment form

--- functions.php
+++ functions.php
@@ -95,24 +95,11 @@ function bones_register_sidebars() {
 // Comment Layout
 function bones_comments($comment, $args, $depth) {
    $GLOBALS['comment'] = $comment; ?>
-	<li <?php comment_class(); ?>>
-		<article id="comment-<?php comment_ID(); ?>" class="clearfix">
+	<li>
+		<article>
 			<header class="comment-author vcard">
-			    <?php 
-			    /*
-			        this is the new responsive optimized comment image. It used the new HTML5 data-attribute to display comment gravatars on larger screens only. What this means is that on larger posts, mobile sites don't have a ton of requests for comment images. This makes load time incredibly fast! If you'd like to change it back, just replace it with the regular wordpress gravatar call:
-			        echo get_avatar($comment,$size='32',$default='<path_to_url>' );
-			    */ 
-			    ?>
-			    <!-- custom gravatar call -->
-			    <?php
-			    	// create variable
-			    	$bgauthemail = get_comment_author_email();
-			    ?>
-			    <img data-gravatar="http://www.gravatar.com/avatar/<?php echo md5($bgauthemail); ?>&s=32" class="load-gravatar avatar avatar-48 photo" height="32" width="32" src="<?php echo get_template_directory_uri(); ?>/library/images/nothing.gif" />
-			    <!-- end custom gravatar call -->
-				<?php printf(__('<cite class="fn">%s</cite>'), get_comment_author_link()) ?>
-				<time datetime="<?php echo comment_time('Y-m-j'); ?>"><a href="<?php echo htmlspecialchars( get_comment_link( $comment->comment_ID ) ) ?>"><?php comment_time('F jS, Y'); ?> </a></time>
+				<?php printf(__('<cite class="fn">%s</cite>'), get_comment_author_link()) ?> on
+				<time datetime="<?php echo comment_time('Y-m-j'); ?>"><a href="<?php echo htmlspecialchars( get_comment_link( $comment->comment_ID ) ) ?>"><?php comment_time('F jS, Y'); ?> </a></time> wrote:
 				<?php edit_comment_link(__('(Edit)', 'bonestheme'),'  ','') ?>
 			</header>
 			<?php if ($comment->comment_approved == '0') : ?>
@@ -120,12 +107,11 @@ function bones_comments($comment, $args, $depth) {
           			<p><?php _e('Your comment is awaiting moderation.', 'bonestheme') ?></p>
           		</div>
 			<?php endif; ?>
-			<section class="comment_content clearfix">
+			<section>
 				<?php comment_text() ?>
 			</section>
-			<?php comment_reply_link(array_merge( $args, array('depth' => $depth, 'max_depth' => $args['max_depth']))) ?>
 		</article>
-    <!-- </li> is added by WordPress automatically -->
+    <?php # </li> is added by WordPress automatically ?>
 <?php
 } // don't remove this bracket!
 
--- single.php
+++ single.php
@@ -1,42 +1,38 @@
 <?php get_header(); ?>
-  <section role="main" class="d2-d4">
+  
+  <section role="main">
 
     <?php while ( have_posts() ) : the_post(); ?>
-      <article id="post-<?php the_ID(); ?>">
-        <header class="entry-header">
-          <h1 class="entry-title"><?php the_title(); ?></h1>
-          <?php if ( 'post' == get_post_type() ) : ?>
-
-          <div class="entry-meta">
-            <div class="post-author">
-              <a href="<?php echo get_the_author_meta( 'user_url' ); ?>" title="Learn more about me"><?php the_author() ?></a>
-            <?php read_time(); ?>
-            </div>
-            <div class="topics">
-              <h3>Topics:</h3> <?php echo get_the_category_list(); ?>
-            </div>
+      <article id="post-<?php the_ID(); ?>" class="index-single" role="article">
+        
+        <div>
+          
+          <header class="entry-header">
+            <h1 class="entry-title"><?php the_title(); ?></h1>
+          </header><?php # .entry-header ?>
+          
+          <div class="entry-content">
+            <?php the_content(); ?>
           </div>
 
-          <?php endif; ?>
-        </header>
-
-        <div class="entry-content">
-          <?php the_content(); ?>
+          <div class="byline vcard">
+            Published by
+            <span class="author"><?php the_author(); ?></span> on
+            <time class="updated" datetime="<?php echo the_time('Y-m-j'); ?>" pubdate><?php the_time(get_option('date_format')); ?></time>
+            <br>Topics:
+            <?php the_category(', '); ?>
+            <?php read_time(); ?>
+          </div>
+          
         </div>
-
-        <div class="entry-utility">
-          <time pubdate datetime="<?php the_time('Y-m-d'); ?>"><?php the_time('F j Y'); ?></time>
-        </div>
+        
       </article>
-  
-      <nav id="nav-blog">
-        <span class="nav-previous"><?php previous_post_link( '%link', __( '<span class="meta-nav">&larr;</span> Previous', 'twentyeleven' ) ); ?></span>
-        <span class="nav-next"><?php next_post_link( '%link', __( 'Next <span class="meta-nav">&rarr;</span>', 'twentyeleven' ) ); ?></span>
-      </nav>
 
       <?php comments_template(); ?>
 
     <?php endwhile; // end of the loop. ?>
+    
+    <?php bones_page_navi(); ?>
 
   </section>
 
