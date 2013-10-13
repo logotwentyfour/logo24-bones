3586691a441fd523c32009898db49d094d456ef6
Patrick O'Hanlon <paddy@paddyohanlon.com>
2013-10-13 11:45:05 +0200

    Clean up header icon links. Add new h5bp icons. Clean up 404. Clean up footer.

--- wp-content/themes/logo24-bones/404.php
+++ wp-content/themes/logo24-bones/404.php
@@ -8,29 +8,9 @@
 
 <head>
   <meta charset="utf-8">
-
   <title>Oops, something went wrong - 404 Page Not Found</title>
-
-  <!-- Google Chrome Frame for IE -->
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-
-  <!-- mobile meta (hooray!) -->
-  <meta name="HandheldFriendly" content="True">
-  <meta name="MobileOptimized" content="320">
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-
-  <!-- For iPhone 4 -->
-  <link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php bloginfo('template_directory'); ?>/library/images/h/apple-touch-icon.png">
-  <!-- For iPad 1-->
-  <link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php bloginfo('template_directory'); ?>/library/images/m/apple-touch-icon.png">
-  <!-- For the new iPad -->
-  <link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php bloginfo('template_directory'); ?>/library/images/h/apple-touch-icon-144x144-precomposed.png">
-  <!-- For iPhone 3G, iPod Touch and Android -->
-  <link rel="apple-touch-icon-precomposed" href="<?php bloginfo('template_directory'); ?>/library/images/l/apple-touch-icon-precomposed.png">
-  <!-- For Nokia -->
-  <link rel="shortcut icon" href="<?php bloginfo('template_directory'); ?>/library/images/l/apple-touch-icon.png">
-  <!-- For everything else -->
-  <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
 
   <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
 
@@ -52,12 +32,8 @@
 </head>
 
 <body class="p404">
-  
-  <hgroup>
-    <h1>404</h1>
-    <h2>Oops! Page Not Found</h2>
-  </hgroup>
+  <h1>404</h1>
+  <h2>Oops! Page Not Found</h2>
   <p>Send an email to <a href="mailto:info@domain.com">info@domain.com</a> to let us know what happened <i>or</i> Try starting again from our <a href="/">home page</a>.</p>
-  
 </body>
 </html>
--- wp-content/themes/logo24-bones/header.php
+++ wp-content/themes/logo24-bones/header.php
@@ -17,33 +17,13 @@
 
   <!-- Google Chrome Frame for IE -->
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-
-  <meta name="HandheldFriendly" content="True">
-  <meta name="MobileOptimized" content="320">
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-
-  <!-- For iPhone 4 -->
-  <link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php bloginfo('template_directory'); ?>/library/images/h/apple-touch-icon.png">
-  <!-- For iPad 1-->
-  <link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php bloginfo('template_directory'); ?>/library/images/m/apple-touch-icon.png">
-  <!-- For the new iPad -->
-  <link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php bloginfo('template_directory'); ?>/library/images/h/apple-touch-icon-144x144-precomposed.png">
-  <!-- For iPhone 3G, iPod Touch and Android -->
-  <link rel="apple-touch-icon-precomposed" href="<?php bloginfo('template_directory'); ?>/library/images/l/apple-touch-icon-precomposed.png">
-  <!-- For Nokia -->
-  <link rel="shortcut icon" href="<?php bloginfo('template_directory'); ?>/library/images/l/apple-touch-icon.png">
-  <!-- For everything else -->
-  <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
 
   <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
 
   <?php # wordpress head functions ?>
   <?php wp_head(); ?>
   <?php # end of wordpress head ?>
-  
-  <!--[if ! lte IE 6]><!-->
-  <!-- Gridset hosted CSS goes here in development -->
-  <!--<![endif]-->
   
   <!--[if lte IE 6]>
   <link rel="stylesheet" href="http://universal-ie6-css.googlecode.com/files/ie6.1.1.css" media="screen, projection">
